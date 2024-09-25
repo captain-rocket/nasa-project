532877585e36077eddad689d8e9569f8c4df5d77
robrown <robrown@stratacache.com>
2024-09-25 14:58:51 -0400

    build(NASA PROJECT): ZTM NodeJS Course
    This is a commit of the NASA-PROJECT code written following along with the ZTM Complete NodeJS course 2024

---
+++ server/src/routes/planets/planets.controller.js
@@ -0,0 +1,8 @@
+const { getAllplanets } = require('../../models/planets.model');
+
+async function httpGetAllplanets(req, res) {
+  return res.status(200).json(await getAllplanets());
+}
+module.exports = {
+  httpGetAllplanets,
+};
