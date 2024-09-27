f3546cf3e6d71dcab5133120446e15163385cc5e
robrown <robrown@stratacache.com>
2024-09-27 11:18:59 -0400

    fix(helmet, cors): helmet in app.js no longer used restored cors
    removed helmet middleware
    restored cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
 const morgan = require('morgan');
 
 const api = require('./routes/api');
@@ -30,11 +29,11 @@ const app = express();
 //   }),
 // );
 
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//   }),
-// );
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 app.use(morgan('combined'));
 
 app.use(express.json());
