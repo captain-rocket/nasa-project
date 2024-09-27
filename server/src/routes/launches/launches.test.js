968721088e5b2862d96b31c678ff3ba7e7f98c59
robrown <robrown@stratacache.com>
2024-09-27 13:50:17 -0400

    fix(ci, tests): github action mongodb and load planets for testing
    Added supercharge/mongodb-github-action for testing
    updated tests to load planets data
    add no testing flag to client package.json currently not tested

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,12 +1,14 @@
 const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 const launchRoute = '/v1/launches';
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
   afterAll(async () => {
     console.log('Disconnecting from MongoDB and closing server...');
